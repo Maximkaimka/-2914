44ad77b821120e3baa4c8943eb555f718a9d2320
Maximkaimka <maximkaimka@gmail.com>
2022-12-26 01:45:14 +0200

    comit дз print

---
+++ 2 класса.py
@@ -0,0 +1,192 @@
+import random
+class Human:
+    def __int__(self, name="Human", job=None, home=None, car=None):
+        self.name = name
+        self.money = 100
+        self.gladness = 50
+        self.satiety = 50
+        self.job = job
+        self.car = car
+        self.home=home
+    def get_home(self):
+        self.home=House()
+    def get_car(self):
+        self.car=Auto(brend_of_car)
+    def get_job(self):
+        if self.car.drive():
+            pass
+        else:
+            self.repair()
+            return
+        self.job=Job(job_list)
+    def eat(self):
+        if self.home.food<-0:
+            self.shoping("food")
+        else:
+            if self.satiety>=100:
+                self.satiety=100
+                return
+            self.satiety+=5
+            self.home.food-=5
+    def work(self):
+        if self.car .drive():
+            pass
+        else:
+            if self.car.fuel<20:
+                self.shoping("fuel")
+                return
+            else:
+                self.to_repair()
+                return
+        else:
+            self.money+=self.job.salary
+            self.gladness-=self.job.gladness_less
+            self.satiety-=4
+    def shoping(self, manage):
+        if self.car.drive():
+            pass
+    else:
+            if self.car.fuel < 20:
+            self.shoping("fuel")
+                return
+            else:
+                self.to_repair()
+                return
+        if manage=="fuel":
+            print("Купив пальне")
+            self.money-=100
+            self.car.fuel+=100
+        elif manage =="food"
+            print("Купив їжу")
+            self.money-=50
+            self.house.food+=50
+        elif manage== "delicacies":
+            print("Смачненьке")
+            self.gladness+=10
+            self.satiety+=2
+            self.money-=15
+    def chill(self):
+        self.gladness+=10
+        self.home.mess+=5
+    def clean_home(self):
+        self.gladness-=5
+        self.home.mess=8
+    def to_repair(self):
+        self.car.strength+=100
+        self.money-=50
+    def day_indexes(self, day):
+        day=f"Сьогодні {day}  {self.name} Життя"
+        print(f"{day:=^50}","\n")
+        human_indexes=self.name+"^s indexes"
+        print(f"{human_indexes:^50}","\n")
+        print(f"Money - {self.money}")
+        print(f"Satiety - {self.satiety}")
+        print(f"Gladness - {self.gladness}")
+        home_indexes="Home indexes"
+        print(f"{home_indexes:^50}","\n")
+        print(f"Food - {self.home.food}")
+        print(f"Mess - {self.home.mess}")
+        car_indexes=f"{self.car.brend} car indexes"
+        print(f"{car_indexes:^50}","\n")
+        print(f"Fuel - {self.car.fuel}")
+        print(f"Strength - {self.car.strength}")
+    def is_alive(self):
+        if self.gladness<0:
+            print("Режим Дед Инсайда")
+            return False
+        if self.satiety<0:
+            print("Голод")
+            return False
+        if self.money<-500:
+            print("Бомж")
+            return False
+    def live(self, day):
+        if self.is_alive()==False:
+            return False
+        if self.home is None:
+            print("Купи хату")
+            self.get_home()
+        if self.car is None
+            self.get_car()
+            print(f"Я купив машину{self.car.brend}")
+        if self.job is None
+            self.get_job()
+            print(f"Я не маю роботи, тому я йду отримувати роботу {self.job.job}з зарплатою {self.job.salary}")
+        self.days_indexes(day)
+        dice=random.randist(1,4)
+        if self..satiety<20:
+        print("Час їсти")
+        self.eat()
+        elif self.gladness<20:
+        if self.home.mess>15:
+            print("Зараз помру,але вдома безлад \n Тож пішов я прибирати")
+            self.clean_home()
+        else:
+            print("Час відпочивати")
+            self.chill()
+        elif self.money<0:
+        print("Час працювати")
+        self.work()
+        elif self.car.strength<15:
+        print("Час ремонтувати машину")
+        self.to_repair()
+        elif dice--1:
+        print("Час відпочивати")
+            self.chill()
+        elif dice--2:
+print("Час працювати")
+self.work()
+elif dice--3:
+print("Зараз помру,але вдома безлад \n Тож пішов я прибирати")
+            self.clean_home()
+elif dice--4:
+print("Час частування")
+self.shoping(manage="delicacies")
+
+
+
+
+class Auto:
+    def __init__(self, brend_list):
+        self.brend=random.choice(list(brend_list))
+        self.fuel=brend_list[self.brend]['fuel']
+        self.strength=brend_list[self.brend]['strenth']
+        self.consuption=brend_list[self.brend]['consuption']
+    brends_of_car={
+        "BMW":{'fuel':100, 'strength':100, 'consuption':6},
+        "Lada":{'fuel':50, 'strength':40, 'consuption':10},
+        "Volvo"{'fuel':70, 'strength':150, 'consuption':8},
+        "Ferrari"{'fuel':80, 'strength':120, 'consuption':14}
+        "Nissan"{'fuel': 90, 'strength': 105, 'consuption': 11}}
+    def drive(self):
+        if self.strength>0 and self.fuel>=self.consuption:
+            self.fuel-=self.consuption
+            self.strength-=1
+            return True
+        else:
+            print("Машина не може рухатись")
+            return False
+class House:
+    def __init__(self):
+        self.ress=0
+        self.food=0
+class Job:
+    def __init__(self, job_list):
+        self.job=random.choice(list(job_list))
+        self.salary=job_list(self.job)("salary")
+        self.gladness_less=job_list(self.job)("gladness_less")
+    job_list={
+        "Java developer":{"salary":50, "gladness":10}
+        "Python developer":{"salary":40, "gladness":3}
+        "C++ developer":{"salary":45, "gladness":25}
+        "Rust developer":{"salary":70, "gladness":1}}
+
+
+
+nick=Human(name="Nick")
+for day in range(1,8):
+    if nick.live(day)==Falce
+        break
+
+
+
